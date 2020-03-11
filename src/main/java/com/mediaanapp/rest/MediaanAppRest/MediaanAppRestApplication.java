3d92c7e69712412404e62a52bbc1a28aff775794
TurtlSandwich <i398785@fhict.nl>
2020-03-11 17:07:10 +0100

    First Commit

---
+++ src/main/java/com/mediaanapp/rest/MediaanAppRest/MediaanAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.mediaanapp.rest.MediaanAppRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaanAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaanAppRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediaanapp/rest/MediaanAppRest/MediaanAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaanapp.rest.MediaanAppRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaanAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
