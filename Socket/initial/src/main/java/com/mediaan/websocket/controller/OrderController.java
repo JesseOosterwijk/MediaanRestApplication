9f349a2f75673297ab1691a44d93dca945bcbdd5
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-05-10 20:59:27 +0200

    Forgot to push

--- Socket/initial/src/main/java/com/mediaan/websocket/controller/OrderController.java
+++ Socket/initial/src/main/java/com/mediaan/websocket/controller/OrderController.java
@@ -9,13 +9,6 @@ import com.mediaan.websocket.model.Order;
 @Controller
 public class OrderController {
 
-//	@MessageMapping("/order")
-//	@SendTo("/topic/orders")
-//	public ReceivedOrder receivedOrder(Order order) throws Exception {
-//		Thread.sleep(1000); // simulated delay
-//		return new ReceivedOrder("Order in, " + HtmlUtils.htmlEscape(order.getName()) + "!");
-//	}
-
 	@MessageMapping("/order")
 	@SendTo("/kitchen")
 	public Order receivedOrder(Order order) throws Exception {
