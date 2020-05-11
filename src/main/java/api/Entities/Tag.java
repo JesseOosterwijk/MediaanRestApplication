16b984af2515b24d29d5abc5fc0d6bd85237f8cc
TurtlSandwich <i398785@fhict.nl>
2020-05-11 08:49:30 +0200

    added jpql relations

--- src/main/java/api/Entities/Tag.java
+++ src/main/java/api/Entities/Tag.java
@@ -2,6 +2,8 @@ package api.Entities;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "Tag")
@@ -15,4 +17,31 @@ public class Tag {
     @NotBlank
     @Column(name = "Name")
     private String name;
+
+    @ManyToMany(mappedBy = "Tags")
+    private Set<Meal> Meals = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Set<Meal> getMeals() {
+        return Meals;
+    }
+
+    public void addMeal(Meal meal) {
+        Meals.add(meal);
+        meal.getTags().add(this);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
